794ca4cdf81a9610679dc5c8079bd5514a364332
Cameron Chandler <webstormcam@gmail.com>
2022-03-19 22:40:17 -0400

    I just gotta figure out how to make mobile..
    I want it so that the mobile menu is the main focus of the screen and you cannot scroll down the page

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 let ham = document.getElementById('ham');
 let drop = document.getElementById('dropdown')
-
+let main = document.getElementById('main-tag')
 
 ham.addEventListener('click',function(){
     if(ham.getAttribute('src') ==='imgs/icons/icon-close.svg'){
         ham.src = 'imgs/icons/icon-hamburger.svg'
         drop.classList.remove('animate__slideInRight')
         drop.classList.add('animate__fadeOut')
-        // drop.style.display='none'
+        main.style.display='grid'
         
         
      
@@ -16,6 +16,7 @@ ham.addEventListener('click',function(){
         drop.style.display='flex'
         drop.classList.add('animate__slideInRight')
         drop.classList.remove('animate__fadeOut')
+        main.style.display='none'
     
         
     }
