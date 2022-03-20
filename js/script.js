420f7ab98f578d19237466cb90c1c5711750d2db
Cameron Chandler <webstormcam@gmail.com>
2022-03-19 22:33:34 -0400

    Lets try this out

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ let drop = document.getElementById('dropdown')
 
 
 ham.addEventListener('click',function(){
-    if(ham.getAttribute('src') ==='imgs/icon-close.svg'){
-        ham.src = 'imgs/icon-hamburger.svg'
-        drop.classList.remove('animate__fadeIn')
+    if(ham.getAttribute('src') ==='imgs/icons/icon-close.svg'){
+        ham.src = 'imgs/icons/icon-hamburger.svg'
+        drop.classList.remove('animate__slideInRight')
         drop.classList.add('animate__fadeOut')
         // drop.style.display='none'
         
@@ -14,7 +14,7 @@ ham.addEventListener('click',function(){
     } else{
         ham.src = 'imgs/icons/icon-close.svg'
         drop.style.display='flex'
-        drop.classList.add('animate__fadeIn')
+        drop.classList.add('animate__slideInRight')
         drop.classList.remove('animate__fadeOut')
     
         
