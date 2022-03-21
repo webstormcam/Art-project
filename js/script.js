e4e52b499674bd4708954384b2a52ee06d8bb18c
Cameron Chandler <webstormcam@gmail.com>
2022-03-20 20:02:33 -0400

    I now have a functioning modal lol

--- js/script.js
+++ js/script.js
@@ -23,3 +23,29 @@ bigX.addEventListener('click',function(){
         drop.style.display ='none'
 }
 });
+
+// create references to the modal...
+var modal = document.getElementById('myModal');
+// to all images -- note I'm using a class!
+var images = document.getElementsByClassName('gallery__img');
+// the image in the modal
+var modalImg = document.getElementById("img01");
+// and the caption in the modal
+var captionText = document.getElementById("caption");
+
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  // and attach our click listener for this image.
+  img.onclick = function(evt) {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
